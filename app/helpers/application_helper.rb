43b107f7020f20a96e55714f3734f1921076f827
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-25 09:39:59 +0000

    Add ApplicatioHelper#display_side_nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	# def session_check_for(current_user)
+  #   if current_user
+  #     render 'shared/user_info'
+
+  #   else
+  #     link_to 'Sign in', user_session_path
+  #   end
+	# end
+	def display_side_nav
+		render '/shared/left_side_navbar' if logged_in?
+	end
 end
