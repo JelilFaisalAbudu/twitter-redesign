c20d0ba32e440cc7326b95059efa44bfc6b9492d
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-26 19:08:47 +0000

    Define helper method to display unfollow button

--- app/helpers/followings_helper.rb
+++ app/helpers/followings_helper.rb
@@ -5,7 +5,11 @@ module FollowingsHelper
 		end
 	end
 
-	
+	def display_unfollow_button_for(user)
+		if user != current_user && following?(user)
+			link_to '<i class="fas fa-minus"></i>'.html_safe, unfollow_user_path(Follower_id: current_user, Followed_id: user), method: :delete
+		end
+	end
 	private
 
 	def following?(user)
