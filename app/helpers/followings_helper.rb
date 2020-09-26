b752fac741cfed6a0a4862c8e0d49cd78bc4abfd
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-26 19:07:18 +0000

    Define helper method to display follow button

--- app/helpers/followings_helper.rb
+++ app/helpers/followings_helper.rb
@@ -1,2 +1,14 @@
 module FollowingsHelper
+	def display_follow_button_for(user)
+		if user != current_user && !following?(user)
+		link_to '<i class="fas fa-plus"></i>'.html_safe, follow_user_path(Followed_id: user), method: :post
+		end
+	end
+
+	
+	private
+
+	def following?(user)
+		current_user.following.include?(user)
+	end
 end
