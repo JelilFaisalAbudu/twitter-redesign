ca3205aaa8254fde5bf30a14663e6be243d9c6fc
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-27 22:02:14 +0000

    Add spec to models/following_spec

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Following, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'ActiveRecord associations' do
+    it 'belongs to follower' do
+      user = Following.reflect_on_association(:Follower)
+      expect(user.macro).to eq(:belongs_to)
+    end
+
+    it 'belongs to followed' do
+      user = Following.reflect_on_association(:Followed)
+      expect(user.macro).to eq(:belongs_to)
+    end
+  end
+
+  context 'Attributes Validation' do
+    
+      user_one = User.create(Username: 'JaneDoe',
+        FullName: 'Jane Doe',
+        Photo: 'photo.jpg',
+        CoverImage: 'image.jpg'
+      )
+  
+      user_two = User.create(Username: 'JohnDoe',
+        FullName: 'John Doe',
+        Photo: 'photo.jpg',
+        CoverImage: 'image.jpg'
+      )
+
+    following = user_one.active_relationships.build(Followed_id: nil)
+
+    it 'should invalidate the Followed_id of value nil' do
+      expect(following).to_not be_valid
+    end
+
+    it 'should validate the presence of the values for user_id and friend_id' do
+      following = user_one.active_relationships.build(Followed_id: user_two.id)
+      expect(following).to be_valid
+    end
+  end
 end
