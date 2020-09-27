3f501043983a33e5585c4d4e10797e34e5603aca
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-27 14:23:54 +0000

    Add follow/unfollow icons in

--- app/helpers/followings_helper.rb
+++ app/helpers/followings_helper.rb
@@ -1,13 +1,14 @@
 module FollowingsHelper
 	def display_follow_button_for(user)
 		if user != current_user && !following?(user)
-		link_to '<i class="fas fa-plus"></i>'.html_safe, follow_user_path(Followed_id: user), method: :post
+		link_to '<i class="fas fa-plus" data-toggle="tooltip" data-placement="top" title="Follow"></i>'.html_safe, follow_user_path(Followed_id: user), method: :post
 		end
 	end
 
 	def display_unfollow_button_for(user)
 		if user != current_user && following?(user)
-			link_to '<i class="fas fa-minus"></i>'.html_safe, unfollow_user_path(Follower_id: current_user, Followed_id: user), method: :delete
+			link_to '<i class="fas fa-minus" data-toggle="tooltip" data-placement="top" title="Unfollow">
+			</i>'.html_safe, unfollow_user_path(Follower_id: current_user, Followed_id: user), method: :delete
 		end
 	end
 	private
