46aaf348806540c5b8c7f2505c107c2cc5d044e2
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-27 23:02:01 +0000

    Add unit test to User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'ActiveRecord associations' do
+    it 'belongs to author' do
+      user = User.reflect_on_association(:tweets)
+      expect(user.macro).to eq(:has_many)
+    end
+  end
+
+  context "Every available user" do
+    user = User.new(
+      Username: 'ExampleUser',
+      FullName: 'Example User',
+      Photo: 'photo.jpg',
+      CoverImage: 'image.jpg'
+    )
+    
+    it "has both username full name" do
+      expect(user).to  be_valid
+    end
+    
+    it "has both username full name" do
+      user.Username = nil
+      expect(user).not_to  be_valid
+    end
+  end
+  
 end
