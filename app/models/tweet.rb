a61b009d862c2c4d58705b27e0c238297a2f3924
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-27 22:30:57 +0000

    Validate the presence of Text in Tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :Author, class_name: 'User'
+
+  validates :Text, presence: true, length: {maximum: 250, minimum: 5}
+  
+  
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Every properly written tweet" do
+    user = User.create(Username: 'JohnDoe',
+      FullName: 'John Doe',
+      Photo: 'photo.jpg',
+      CoverImage: 'image.jpg'
+    )
+    it "has the presence of text" do
+      tweet = user.tweets.build(Text: 'Lorem ipsum')
+      expect(tweet).to  be_valid
+    end
+  end
+
+  context "Every properly written tweet" do
+    user = User.create(Username: 'JohnDoe',
+      FullName: 'John Doe',
+      Photo: 'photo.jpg',
+      CoverImage: 'image.jpg'
+    )
+    it "has the presence of text" do
+      tweet = user.tweets.build(Text: nil)
+      expect(tweet).not_to  be_valid
+    end
+  end
+  
+  context 'ActiveRecord associations' do
+    it 'belongs to author' do
+      user = Tweet.reflect_on_association(:Author)
+      expect(user.macro).to eq(:belongs_to)
+    end
+  end
+  
 end
