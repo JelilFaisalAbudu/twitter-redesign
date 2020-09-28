922ca0342c1eaf61214d879a48bb37dec7fc754f
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-28 13:42:04 +0000

    Add integration spec for user

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,24 +1,40 @@
 require 'rails_helper'
 
 RSpec.feature 'Users', type: :feature do
-  describe 'GET /new' do
-    it 'returns http success' do
-      get '/users/new'
-      expect(response).to have_http_status(:success)
-    end
+  let(:jane) do
+    FactoryBot.create(:user)
+  end
+  
+  let(:john) do
+    User.create(
+      Username: 'JohnDoe',
+      FullName: 'John Doe',
+      Photo: 'photo.jpg',
+      CoverImage: 'coverImage.jpg'
+    )
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
-      expect(response).to have_http_status(:success)
+  before do
+    visit sign_in_path
+
+    within('form') do
+      fill_in "username",	with: jane.Username 
     end
+    click_button 'Submit'
   end
 
-  describe 'GET /edit' do
-    it 'returns http success' do
-      get '/users/edit'
-      expect(response).to have_http_status(:success)
-    end
+  scenario 'as new newly registered have no followers' do
+    expect(jane.followers.count).to eq(0) 
   end
+
+  scenario 'as new newly registered have no following' do
+    expect(jane.following.count).to eq(0) 
+  end
+
+  scenario 'as new newly registered can follow other users' do
+     jane.active_relationships.create(Followed_id: john.id)
+     expect(jane.following.count).to  eq(1)
+     expect(john.followers.count).to  eq(1)
+  end
+
 end
