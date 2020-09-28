f32d9d6f88d88aeae55c1dfa71ba86d4ecf97666
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-28 10:10:04 +0000

    Add integration spec for the sessions

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -1,10 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe 'Sessions', type: :feature do
-  describe 'GET /new' do
-    it 'returns http success' do
-      get '/sessions/new'
-      expect(response).to have_http_status(:success)
+RSpec.feature 'Sessions', type: :feature do
+  let(:user) { FactoryBot.create(:user) }
+  context "visit the webpage" do
+    scenario 'should display the login page' do
+      visit 'tweets/index'
+      expect(page).to  have_content('Sign in')
+    end
+
+    scenario 'should not sign in unregistered user' do
+      visit sign_in_path
+
+      within('form') do
+        fill_in "username",	with: "InvalidUserName" 
+      end
+      click_button 'Submit'
+      expect(page).to  have_content('Invalid username. Try again')
+    end
+
+    scenario 'should sign in registered user' do
+      visit sign_in_path
+
+      within('form') do
+        fill_in "username",	with: user.Username 
+      end
+      click_button 'Submit'
+      expect(page).to  have_content('Start a new tweet')
     end
   end
 end
