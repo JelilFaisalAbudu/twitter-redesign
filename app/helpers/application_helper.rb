5049570db1b24a60140c83e97f7618f2c91e4915
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-09-29 10:02:46 +0000

    Define helper methods to display avatar and cover image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,31 @@ module ApplicationHelper
       render 'shared/signin_or_signup'
     end
   end
+
+  def avatar_for(user)
+    avatar = user.photo
+    if avatar.attached?
+      image_tag(avatar, alt: "@#{user.Username}", class: 'profile-pic')
+    else
+      image_tag('default_avatar.png', alt: "@#{user.Username}", class: 'profile-pic')
+    end
+  end
+
+  def cover_image_for(user)
+    cover_image = user.cover_image
+    if cover_image.attached?
+      image_tag(cover_image, alt: "@#{user.Username} ", width: 1046, height: 302)
+    else
+      image_tag('default_cover.jpg', alt: "@#{user.Username}", width: 1046, height: 300)
+    end
+  end
+
+  # def cover_image(user)
+  #   if user.cover_image.attached?
+
+  #     url_for(user.cover_image)
+  #   else
+  #     url_for('default_cover.jpg')
+  #   end
+  # end
 end
